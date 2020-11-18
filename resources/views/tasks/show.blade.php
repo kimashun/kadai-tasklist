4d623b87ddad32db9bb0532019bc71e9c6f13549
shun.kima <shun.kima@techacademy.jp>
2020-11-18 12:45:51 +0000

    CRUD tasks

---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>id = {{ $task->id }} の詳細ページ</h1>
+
+    <table class="table table-bordered">
+        <tr>
+            <th>id</th>
+            <td>{{ $task->id }}</td>
+        </tr>
+        <tr>
+            <th>メッセージ</th>
+            <td>{{ $task->content }}</td>
+        </tr>
+    </table>
+    {{-- メッセージ編集ページへのリンク --}}
+    {!! link_to_route('tasks.edit', 'このタスクを編集', ['task' => $task->id], ['class' => 'btn btn-light']) !!}
+
+@endsection
